c168c1eedd44ef1531705e0b7a9e3a79ca2ec95f
Walton Pelkey <waltonpelkey@gmail.com>
2026-04-21 12:45:38 -0400

    Initial Commit with working computer shader for Day and Night rules of Game of Life.

---
+++ main.js
@@ -0,0 +1,32 @@
+import { default as seagulls } from '/gulls.js'
+
+const sg      = await seagulls.init(),
+      frag    = await seagulls.import( './frag.wgsl' ),
+      compute = await seagulls.import( './compute.wgsl' ),
+      render  = seagulls.constants.vertex + frag,
+      size    = (window.innerWidth * window.innerHeight),
+      state   = new Float32Array( size )
+
+for( let i = 0; i < size; i++ ) {
+  state[ i ] = Math.round( Math.random() )
+}
+
+const statebuffer1 = sg.buffer( state )
+const statebuffer2 = sg.buffer( state )
+const res = sg.uniform([ window.innerWidth, window.innerHeight ])
+
+const renderPass = await sg.render({
+  shader: render,
+  data: [
+    res,
+    sg.pingpong( statebuffer1, statebuffer2 )
+  ]
+})
+
+const computePass = sg.compute({
+  shader: compute,
+  data: [ res, sg.pingpong( statebuffer1, statebuffer2 ) ],
+  dispatchCount:  [Math.round(seagulls.width / 8), Math.round(seagulls.height/8), 1],
+})
+
+sg.run( computePass, renderPass )
